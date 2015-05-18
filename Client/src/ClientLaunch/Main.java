bf44a4e1ef1107e7c858ee74f919ca33c0dcc5d8
Jeroentjuh99 <jeroenvanos2@gmail.com>
2015-05-18 12:40:03 +0200

    upload 2

--- Client/src/ClientLaunch/Main.java
+++ Client/src/ClientLaunch/Main.java
@@ -9,6 +9,11 @@ import javax.swing.JFrame;
 public class Main extends JFrame {
 
     public static void main(String[] args) {
+	JFrame frame = new JFrame("Windows Live Messenger");
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+	frame.setSize(600, 800);
+	frame.setResizable(false);
+	frame.setVisible(true);
     }
-}
+} 
