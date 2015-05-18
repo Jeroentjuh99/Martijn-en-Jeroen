999a46d3a0a0a346f3b910ed15f86a9ad445363c
Jeroentjuh99 <jeroenvanos2@gmail.com>
2015-05-18 11:31:26 +0200

    launchklassen gemaakt

---
+++ Client/src/ClientLaunch/Main.java
@@ -0,0 +1,14 @@
+package ClientLaunch;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author jeroen
+ */
+public class Main extends JFrame {
+
+    public static void main(String[] args) {
+
+    }
+}
