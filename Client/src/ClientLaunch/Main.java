02325b292a6e1fec5ed7fb2a4dda31dd8e756c79
m <martijncaluwe@gmail.com>
2015-06-03 16:36:52 +0200

    nog een keer dan

--- Client/src/ClientLaunch/Main.java
+++ Client/src/ClientLaunch/Main.java
@@ -7,10 +7,14 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.Toolkit;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JEditorPane;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -21,7 +25,7 @@ import javax.swing.JTextField;
  * @author jeroen
  */
 public class Main extends JFrame {
-
+	private JPanel panel = new JPanel();
 	public static void main(String[] args) {
 		Main m = new Main();
 
@@ -29,12 +33,17 @@ public class Main extends JFrame {
 
 	public Main() {
 		super("Windows Live Messenger");
-		Menu menu = new Menu(this);
+		Menu menu = new Menu(this,panel);
 		if(menu.gebruikersnaam())
 		{
+		setSize(600, 800);
 		JPanel backgroundSouth = new JPanel(new FlowLayout(FlowLayout.LEFT));
-		JPanel panel = new JPanel();
-		JTextArea bericht = new JTextArea(5,40);
+//		JPanel panel = new JPanel();
+		//JTextArea chat = new JTextArea();
+		//chat.setSize(panel.getWidth(), panel.getHeight());
+		System.out.println(panel.getWidth());
+//		panel.add(chat);
+		JTextArea bericht = new JTextArea(5,40); 
 		JScrollPane scrollen = new JScrollPane(bericht, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		backgroundSouth.add(scrollen);
 		backgroundSouth.add(menu.Send(bericht));
@@ -42,10 +51,25 @@ public class Main extends JFrame {
 		add(backgroundSouth, BorderLayout.SOUTH);
 		getContentPane().add(panel);
 		//panel.setBackground(Color.black);
-		setSize(600, 800);
 		setResizable(false);
 		setVisible(true);
+		
+//		ServerConnection c = new ServerConnection(menu, panel);
+		
+		System.out.println(panel.getHeight());
 		}
 	}
+	
+	public void volgendeRegel(final JTextArea bericht)
+	{
+		bericht.addKeyListener(new KeyAdapter() {
+			public void keyTyped(KeyEvent e) {
+				bericht.setWrapStyleWord(true);
+				bericht.setLineWrap(true);
+			}
+		});
+
+	}
+
 
 }
--- Client/src/ClientLaunch/ServerConnection.java
+++ Client/src/ClientLaunch/ServerConnection.java
@@ -6,25 +6,45 @@ import java.net.Socket;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class ServerConnection {
+public class ServerConnection implements Runnable{
 
 	private Menu menu;
 	private JPanel panel;
 	private String[] ip;
+	private DataOutputStream toServer;
+	private DataInputStream fromServer;
 
 	public ServerConnection(Menu menu, JPanel p) {
 		this.menu = menu;
 		this.panel = p;
+		
+		
+		Thread t = new Thread(this);
+		t.start();
 	}
 
-	public void sendMessage() {
-
+	public void sendMessage(String message) {
+		
 	}
 
-	public void getMessage(String text) {
-		JLabel message = new JLabel();
-		message.setText(text);
-		panel.add(message);
+	public void getMessage() {
+		String text=null;
+		try {
+			text = fromServer.readUTF();
+			if(text.equals("leef je nog woord"))
+			{
+				sendMessage("ik leef nog");
+			}
+			else if(!text.isEmpty())
+			{
+			JLabel message = new JLabel();
+			message.setText(text);
+			panel.add(message);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public boolean serverConnection(String gebruikersnaam, String ipadres) {
@@ -33,12 +53,12 @@ public class ServerConnection {
 			ip = ipadres.split(";");
 			Socket socket = new Socket(ip[0], Integer.parseInt(ip[1]));
 			// Create an input stream to receive data from the server
-			// fromServer = new DataInputStream(
-			// socket.getInputStream());
+			 fromServer = new DataInputStream(
+			 socket.getInputStream());
 
 			// Create an output stream to send data to the server
-			// toServer =
-			// new DataOutputStream(socket.getOutputStream());
+			toServer =
+			new DataOutputStream(socket.getOutputStream());
 			connectie = true;
 			return connectie;
 //			System.out.println("sdfsdfhaopisfdhafdpoahogfs");
@@ -53,6 +73,21 @@ public class ServerConnection {
 
 		// if()
 		// ServerConnection c = new ServerConnection(menu, panel);
+	}
+
+	@Override
+	public void run() {
+		while(true){	
+			try {
+				
+				getMessage();
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
 	}
 
 }
