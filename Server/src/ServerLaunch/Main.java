200418c94e421175b09a4b2d3748a3830810d796
Jeroentjuh99 <jeroenvanos2@gmail.com>
2015-06-07 22:30:41 +0200

    yay

--- Server/src/ServerLaunch/Main.java
+++ Server/src/ServerLaunch/Main.java
@@ -15,20 +15,20 @@ import javax.swing.*;
  * @author jeroen
  */
 public class Main extends JFrame {
-    
+
     private final Color textColor = Color.GREEN;
     private ServerContent s = null;
-    
+
     public static void main(String[] args) {
 	Main m = new Main();
     }
-    
+
     public Main() {
 	super("Chatservice Server");
 	setLayout(new BorderLayout());
 	setSize(720, 600);
 	setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-	
+
 	JTextArea textOut = setTextArea(new JTextArea());
 	JTextField textIn = setTextField(new JTextField());
 	JButton button = new JButton();
@@ -36,17 +36,17 @@ public class Main extends JFrame {
 	getRootPane().setDefaultButton(button);
 	this.s = new ServerContent(textOut, textIn);
 	JScrollPane pane = new JScrollPane(textOut, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-	
+
 	button.setVisible(false);
-	
+
 	add(pane, BorderLayout.CENTER);
 	add(textIn, BorderLayout.SOUTH);
 	add(button, BorderLayout.NORTH);
-	
+
 	setResizable(false);
 	setVisible(true);
     }
-    
+
     private JTextArea setTextArea(JTextArea textOut) {
 	textOut.setEditable(false);
 	textOut.setWrapStyleWord(true);
@@ -56,36 +56,37 @@ public class Main extends JFrame {
 	textOut.setForeground(textColor);
 	return textOut;
     }
-    
+
     private JTextField setTextField(JTextField textIn) {
 	textIn.setBackground(Color.BLACK);
 	textIn.setFont(new Font("Lucida Console", Font.BOLD, 13));
 	textIn.setForeground(textColor);
+	textIn.setCaretColor(textColor);
+	textIn.requestFocus();
 	return textIn;
     }
-    
+
     private class buttonListener implements ActionListener {
-	
+
 	private final JTextField textIn;
-	
+
 	public buttonListener(JTextField textIn) {
 	    this.textIn = textIn;
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 	    String a = textIn.getText();
+	    s.logText(a);
 	    if (a.equalsIgnoreCase("/shutdown")) {
 		if (!(s == null)) {
-		    s.logText(a);
 		    s.sluitLogger();
 		}
 		System.exit(0);
-	    } else if (a.equalsIgnoreCase("/ip")){
-		s.logText(a);
+	    } else if (a.equalsIgnoreCase("/ip")) {
 		s.showTextFromCommand("ip");
 	    }
-	    
+
 	    textIn.setText(null);
 	}
     }
