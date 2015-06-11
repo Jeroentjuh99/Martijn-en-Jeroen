239acf242f3107779fc6fffdf1b1dcf9b2ba9522
Jeroentjuh99 <jeroenvanos2@gmail.com>
2015-06-11 21:10:20 +0200

    debug

--- Server/src/ServerContent/Client.java
+++ Server/src/ServerContent/Client.java
@@ -57,6 +57,7 @@ public class Client implements Runnable {
 		    for (int i = 0; i < maxClients; i++) {
 			if (!(clients[i] == null)) {
 			    clients[i].output.writeUTF(text);
+			    System.out.println(text);
 			}
 		    }
 		}
