eb99975a2e08bd7a8644c2dc3d8c69d52acbbe0c
Jeroentjuh99 <jeroenvanos2@gmail.com>
2015-06-13 22:14:38 +0200

    ben lekker bezig :satisfied:

--- Server/src/ServerContent/ServerContent.java
+++ Server/src/ServerContent/ServerContent.java
@@ -25,7 +25,7 @@ public class ServerContent implements Runnable {
     private int maxClients = 10;
     private Client[] clients = new Client[maxClients];
     private ServerLog logger;
-    private String[] validCommands = {"/shutdown", "/quit", "/exit", "/ip", "/say ", "/help", "/showUsers", "/showIPs"};
+    private String[] validCommands = {"/shutdown, /quit, /exit", "/ip", "/say your_text_here", "/help", "/showUsers", "/showIPs", "/kick username_here"};
 
     public ServerContent(JTextArea textOut) {
 	this.textOut = textOut;
@@ -55,7 +55,7 @@ public class ServerContent implements Runnable {
 
     public void handleCommand(String text) {
 	try {
-	    if (text.equalsIgnoreCase(validCommands[0]) || text.equalsIgnoreCase(validCommands[1]) || text.equalsIgnoreCase(validCommands[2])) {
+	    if (text.equalsIgnoreCase(validCommands[0].substring(0, 9)) || text.equalsIgnoreCase(validCommands[0].substring(11, 16)) || text.equalsIgnoreCase(validCommands[0].substring(18, 23))) {
 		validCommand(text);
 		for (int i = 0; i < maxClients; i++) {
 		    if (!(clients[i] == null)) {
@@ -65,27 +65,30 @@ public class ServerContent implements Runnable {
 		closeLog();
 		System.exit(0);
 
-	    } else if (text.equalsIgnoreCase(validCommands[3])) {
+	    } else if (text.equalsIgnoreCase(validCommands[1])) {
 		validCommand(text);
 		writeToScreen("IP: " + InetAddress.getLocalHost() + ":" + server.getLocalPort());
 		logText(InetAddress.getLocalHost() + ":" + server.getLocalPort());
 
-	    } else if (text.startsWith(validCommands[4])) {
+	    } else if (text.startsWith(validCommands[2].substring(0, 5))) {
 		validCommand(text);
 		for (int i = 0; i < maxClients; i++) {
 		    if (!(clients[i] == null)) {
 			clients[i].messageFromServer(text);
 		    }
 		}
-	    } else if (text.equalsIgnoreCase(validCommands[5])) {
+	    } else if (text.equalsIgnoreCase(validCommands[3])) {
 		validCommand(text);
 		Help();
-	    } else if (text.equalsIgnoreCase(validCommands[6])) {
+	    } else if (text.equalsIgnoreCase(validCommands[4])) {
 		validCommand(text);
 		showConnectedClients();
-	    } else if (text.equalsIgnoreCase(validCommands[7])) {
+	    } else if (text.equalsIgnoreCase(validCommands[5])) {
 		validCommand(text);
 		showConnectedIPs();
+	    } else if (text.startsWith(validCommands[6].substring(0, 6))) {
+		validCommand(text);
+		kickUser(text.substring(6));
 	    } else {
 		Help();
 	    }
@@ -134,14 +137,14 @@ public class ServerContent implements Runnable {
     }
 
     private void Help() {
-	String t = "The valid commands are:\n";
+	String t = "The valid commands are: \n";
 	int a = 0;
 	int i = 0;
 	for (i = 0; i < validCommands.length; i++) {
 	    a = i + 1;
-	    t += a + ". \"" + validCommands[i] + "\"\n";
+	    t += a + ". \"" + validCommands[i] + "\" \n";
 	}
-	t += "without the \"";
+	t += "without the \" ";
 	validCommand(t);
     }
 
@@ -153,11 +156,14 @@ public class ServerContent implements Runnable {
 	    }
 	}
 	Collections.sort(clientsList);
-	String t = "The connected users are:\n";
+	String t = "The connected users are: \n";
 	int i = 1;
 	for (Client c : clientsList) {
-	    t += i + ". " + c.getClientName() + "\n";
+	    t += i + ". " + c.getClientName() + " \n";
 	    i++;
+	}
+	if (t.equals("The connected users are: \n")) {
+	    t = "There are no clients connected";
 	}
 	validCommand(t);
     }
@@ -170,12 +176,40 @@ public class ServerContent implements Runnable {
 	    }
 	}
 	Collections.sort(clientsList, Client.SortOnIp());
-	String t = "The connected IPs are:\n";
+	String t = "The connected IPs are: \n";
 	int i = 1;
 	for (Client c : clientsList) {
-	    t += i + ". " + c.getIP()+ "\n";
+	    t += i + ". " + c.getIP() + " \n";
 	    i++;
 	}
+	if (t.equals("The connected IPs are: \n")) {
+	    t = "There are no clients connected";
+	}
 	validCommand(t);
+    }
+
+    private void kickUser(String user) {
+	String banHammer = null;
+	for (int i = 0; i < clients.length; i++) {
+	    if (!(clients[i] == null)) {
+		if (clients[i].getClientName().equalsIgnoreCase(user)) {
+		    banHammer = clients[i].getIP();
+		}
+	    }
+	}
+
+	for (int i = 0; i < clients.length; i++) {
+	    if (!(clients[i] == null)) {
+		if (clients[i].getIP().equalsIgnoreCase(banHammer)) {
+		    try {
+			clients[i].messageFromServer("/say The BanHammer has spoken!");
+			clients[i].messageFromServer("/shutdown");
+			validCommand(clients[i].getClientName() + " has been kicked");
+			clients[i] = null;
+		    } catch (IOException e) {
+		    }
+		}
+	    }
+	}
     }
 }
