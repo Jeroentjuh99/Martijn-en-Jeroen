c3116dd7711dcf5df4b9fa1bf26da7d312be7fac
Jeroentjuh99 <jeroenvanos2@gmail.com>
2015-06-13 23:29:14 +0200

    bepaal ipv pepaal

--- Client/src/ClientLaunch/Recursiegame.java
+++ Client/src/ClientLaunch/Recursiegame.java
@@ -42,7 +42,7 @@ class recursiegame1 extends JFrame {
 	path.addActionListener(new ActionListener() {
 	    public void actionPerformed(ActionEvent event) {
 		foundPath = false;
-		if (pepaalPath(0, 0, 7, 7)) {
+		if (bepaalPath(0, 0, 7, 7)) {
 		    JOptionPane.showMessageDialog(null, "Path Found");
 		} else {
 		    JOptionPane.showMessageDialog(null, "Path not Found");
@@ -81,7 +81,7 @@ class recursiegame1 extends JFrame {
 	setVisible(true);
     }
 
-    public Boolean pepaalPath(int bCol, int bRow, int eCol, int eRow) {
+    public Boolean bepaalPath(int bCol, int bRow, int eCol, int eRow) {
 	p++;
 	buttons.get((bRow * 8) + bCol).setBackground(Color.BLACK);
 	buttons.get((bRow * 8) + bCol).setText(String.valueOf(p));
@@ -89,19 +89,19 @@ class recursiegame1 extends JFrame {
 	    foundPath = true;
 	    return foundPath;
 	} else if (bRow < 7 && (buttons.get(((bRow + 1) * 8) + bCol)).getText() != "x" && buttons.get(((bRow + 1) * 8) + bCol).getBackground() != Color.BLACK) {
-	    pepaalPath(bCol, bRow + 1, eCol, eRow);
+	    bepaalPath(bCol, bRow + 1, eCol, eRow);
 	} else if (bCol < 7 && (buttons.get((bRow * 8) + bCol + 1)).getText() != "x" && buttons.get((bRow * 8) + bCol + 1).getBackground() != Color.BLACK) {
-	    pepaalPath(bCol + 1, bRow, eCol, eRow);
+	    bepaalPath(bCol + 1, bRow, eCol, eRow);
 	} else if (bRow > 0 && (buttons.get(((bRow - 1) * 8) + bCol)).getText() != "x" && buttons.get(((bRow - 1) * 8) + bCol).getBackground() != Color.BLACK) {
-	    pepaalPath(bCol, bRow - 1, eCol, eRow);
+	    bepaalPath(bCol, bRow - 1, eCol, eRow);
 	} else if (bCol > 0 && (buttons.get((bRow * 8) + bCol - 1)).getText() != "x" && buttons.get((bRow * 8) + bCol - 1).getBackground() != Color.BLACK) {
-	    pepaalPath(bCol - 1, bRow, eCol, eRow);
+	    bepaalPath(bCol - 1, bRow, eCol, eRow);
 	} else if (bCol > 0 && buttons.get(bCol - 1 + (bRow * 8)).getBackground() == Color.BLACK) {
 	    buttons.get(bCol - 1 + (bRow * 8)).setBackground(null);
-	    pepaalPath(bCol - 1, bRow, eCol, eRow);
+	    bepaalPath(bCol - 1, bRow, eCol, eRow);
 	} else if (bRow > 0 && buttons.get(bCol + (bRow - 1) * 8).getBackground() == Color.BLACK) {
 	    buttons.get(bCol + (bRow - 1) * 8).setBackground(null);
-	    pepaalPath(bCol, bRow - 1, eCol, eRow);
+	    bepaalPath(bCol, bRow - 1, eCol, eRow);
 	} else {
 	    foundPath = false;
 	}
