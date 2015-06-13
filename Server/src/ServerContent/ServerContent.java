caf80fccde8c769c8986adaaf4b1acb7fc3098bc
Jeroentjuh99 <jeroenvanos2@gmail.com>
2015-06-13 17:09:51 +0200

    yay for me! :confetti_ball:

--- Server/src/ServerContent/ServerContent.java
+++ Server/src/ServerContent/ServerContent.java
@@ -23,14 +23,17 @@ public class ServerContent implements Runnable {
     private int maxClients = 10;
     private Client[] clients = new Client[maxClients];
     private ServerLog logger;
+    private String[] validCommands = {"/shutdown", "/quit", "/exit", "/ip", "/say ", "/help"};
 
     public ServerContent(JTextArea textOut) {
 	this.textOut = textOut;
 	try {
 	    this.server = new ServerSocket(0);
-	    writeToScreen(InetAddress.getLocalHost() + ":" + server.getLocalPort());
+	    String ipString = InetAddress.getLocalHost() + ":" + server.getLocalPort();
+	    writeToScreen(ipString);
+	    writeToScreen("Type /help for all available commands");
 	    this.logger = new ServerLog();
-	    logger.addText("IP: " + InetAddress.getLocalHost() + ":" + server.getLocalPort());
+	    logger.addText("IP: " + ipString);
 
 	    Thread t = new Thread(this);
 	    t.start();
@@ -50,29 +53,32 @@ public class ServerContent implements Runnable {
 
     public void handleCommand(String text) {
 	try {
-	    if (text.equalsIgnoreCase("/shutdown") || text.equalsIgnoreCase("/quit") || text.equalsIgnoreCase("/exit")) {
+	    if (text.equalsIgnoreCase(validCommands[0]) || text.equalsIgnoreCase(validCommands[1]) || text.equalsIgnoreCase(validCommands[2])) {
 		validCommand(text);
 		for (int i = 0; i < maxClients; i++) {
 		    if (!(clients[i] == null)) {
-			clients[i].messageFromServer("/shutdown");
+			clients[i].messageFromServer(validCommands[0]);
 		    }
 		}
 		closeLog();
 		System.exit(0);
 
-	    } else if (text.equalsIgnoreCase("/ip")) {
+	    } else if (text.equalsIgnoreCase(validCommands[3])) {
 		validCommand(text);
 		writeToScreen("IP: " + InetAddress.getLocalHost() + ":" + server.getLocalPort());
 		logText(InetAddress.getLocalHost() + ":" + server.getLocalPort());
 
-	    } else if (text.startsWith("/say ")) {
+	    } else if (text.startsWith(validCommands[4])) {
 		validCommand(text);
 		for (int i = 0; i < maxClients; i++) {
 		    if (!(clients[i] == null)) {
 			clients[i].messageFromServer(text);
 		    }
 		}
-
+	    } else if (text.equalsIgnoreCase(validCommands[5])) {
+		Help();
+	    } else {
+		Help();
 	    }
 	} catch (Exception e) {
 	}
@@ -113,7 +119,20 @@ public class ServerContent implements Runnable {
 	}
     }
 
-    void messageFromClient(String text) {
+    public void messageFromClient(String text) {
+	text = text.substring(5);
 	validCommand(text);
+    }
+
+    private void Help() {
+	String t = "The valid commands are:\n";
+	int a = 0;
+	int i = 0;
+	for (i = 0; i < validCommands.length; i++) {
+	    a = i + 1;
+	    t += a + ". \"" + validCommands[i] + "\"\n";
+	}
+	t += "without the \"";
+	validCommand(t);
     }
 }
